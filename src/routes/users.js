4bcfc00e10e1c2fae3403b0e41db6c636d21e068
Nevirone <nevirone@proton.me>
2023-08-12 12:37:34 +0200

    Fix

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,15 +10,15 @@ router.get('/', async (req, res) => {
 
     res.status(200).send(users)
   }
-  catch(err) {
+  catch (err) {
     console.error(err)
     res.status(500).send()
   }
 })
 
 router.post('/', async (req, res) => {
-  if(!req.body.username)
-    return res.status(400).send({ message: 'Username not provided'})
+  if (!req.body.username)
+    return res.status(400).send({ message: 'Username not provided' })
 
   try {
     const user = User({
@@ -28,48 +28,45 @@ router.post('/', async (req, res) => {
     await user.save()
     res.status(201).send(user)
   }
-  catch(err) {
+  catch (err) {
     console.error(err)
     res.status(500).send()
   }
 })
 
 router.post('/:_id/exercises', async (req, res) => {
-  if(!req.body.description)
-    return res.status(400).send({ message: 'Description not provided'})
+  if (!req.body.description)
+    return res.status(400).send({ message: 'Description not provided' })
 
-  if(!req.body.duration)
-    return res.status(400).send({ message: 'Duration not provided'})
+  if (!req.body.duration)
+    return res.status(400).send({ message: 'Duration not provided' })
 
   try {
     const user = await User.findById(req.params._id)
 
-    if(!user) 
+    if (!user)
       return res.status(404).send({ message: 'User with given id not found' })
 
     const exercise = Exercise({
       description: req.body.description,
-      duration: req.body.duration,
+      duration: Number(req.body.duration),
       userId: user._id
     })
 
-    if(req.body.date)
-      exercise.date = req.body.date
+    if (req.body.date)
+      exercise.date = new Date(req.body.date).toDateString()
 
     await exercise.save()
 
-    const response = {
+    res.json({
       username: user.username,
-      _id: user._id,
       description: exercise.description,
       duration: exercise.duration,
       date: exercise.date,
-      userId: exercise.userId
-    }
-
-    res.status(201).send(response)
+      _id: user._id
+    });
   }
-  catch(err) {
+  catch (err) {
     console.error(err)
     res.status(500).send()
   }
@@ -79,38 +76,35 @@ router.get('/:_id/logs', async (req, res) => {
   try {
     const user = await User.findById(req.params._id)
 
-    if(!user) 
+    if (!user)
       return res.status(404).send({ message: 'User with given id not found' })
 
     const exercises = await Exercise.find({ userId: user._id })
 
-    let response = {
-      username: user.username,
-      _id: user._id,
-      count: exercises.count,
-      log: exercises
-    }
+    let response = user.toObject()
+    response.count = exercises.length
+    response.log = exercises
 
     if(req.query.from) {
       const fromDate = new Date(req.query.from)
-      response.log = response.log.filter(obj => {
-        const date = new Date(obj.log.date)
+       response.log = response.log.filter(obj => {
+        const date = new Date(obj.date)
         return date >= fromDate
       })
     }
     
     if(req.query.to) {
-      const toDate = new Date(req.query.to)
-      response.log = response.log.filter(obj => {
-        const date = new Date(obj.log.date)
+  const toDate = new Date(req.query.to)
+       response.log = response.log.filter(obj => {
+        const date = new Date(obj.date)
         return date <= toDate
       })
     }
 
     if(req.query.limit)
       response.log = response.log.splice(0, req.query.limit)
-
-    res.status(200).send(response)
+ 
+    res.json(response)
   }
   catch(err) {
     console.error(err)
